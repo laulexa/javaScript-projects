915d6e47293631a074274b1b9852ed79369c68e3
Laura R <laualexandrar@gmail.com>
2023-03-01 11:47:33 -0800

    attack function, createMessage function and battle function

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -1,6 +1,17 @@
+let playerAttack;
+let opponentAttack;
+let gameResult;
+
 function startGame() {
     let chooseBuddyBtn = document.getElementById("choose-buddy-btn");
     chooseBuddyBtn.addEventListener("click", chooseBuddy);
+    
+    let fireBtn = document.getElementById("fire-btn");
+    fireBtn.addEventListener ("click", fireAttack);
+    let waterBtn = document.getElementById("water-btn");
+    waterBtn.addEventListener ("click", waterAttack);
+    let groundBtn = document.getElementById("ground-btn");
+    groundBtn.addEventListener ("click", groundAttack);
 }
 
 function chooseBuddy() {
@@ -53,5 +64,71 @@ function randomOpponentBuddy() {
 function randomNumber(min,max) {
    return Math.floor(Math.random() * (max - min + 1) + min)
 }
+   // Hipodoge -> Water
+   // Capipepo -> ground
+   // Ratigueya -> fire
+   // Langostelvis -> fire and water
+   // Tucapalma -> ground and water
+   // Pydos -> ground and fire
+
+function fireAttack () {
+   console.log("fire attack")
+   playerAttack = "FIRE"
+   opponentRandomAttack()
+}
+
+function waterAttack () {
+   console.log("water attack")
+   playerAttack = "WATER"
+   opponentRandomAttack()
+}
+
+function groundAttack () {
+   console.log("ground attack")
+   playerAttack = "GROUND"
+   opponentRandomAttack()
+}
+
+function opponentRandomAttack() {
+   let randomBuddyAttack = randomNumber(1,3) 
+   if(randomBuddyAttack == 1) {
+      opponentAttack = "FIRE"
+   } else if (randomBuddyAttack == 2) {
+      opponentAttack = "WATER"
+   } else if (randomBuddyAttack == 3) {
+      opponentAttack = "GROUND"
+   }
+   battle()
+}
+
+function createMessage() {
+   let sectionMessages = document.getElementById("mensajes");
+   let paragraph = document.createElement('p');
+   paragraph.innerHTML = "Your buddy chose: " + playerAttack + ", and your opponent chose: " + opponentAttack + " - You " + gameResult;
+   sectionMessages.appendChild(paragraph)
+}
+
+function battle () {
+   if(playerAttack == opponentAttack) {
+      gameResult = "TIED 🤦🏽‍♀️"
+   } else if(playerAttack == "FIRE" && opponentAttack == "WATER") {
+      gameResult = "LOST 😩"
+   } else if(playerAttack == "FIRE" && opponentAttack == "GROUND") {
+      gameResult = "LOST 😩"
+   }  else if(playerAttack == "WATER" && opponentAttack == "FIRE") {
+      gameResult = "WIN 🏆"
+   } else if(playerAttack == "WATER" && opponentAttack == "GROUND") {
+      gameResult = "WIN 🏆 "
+   }  else if(playerAttack == "GROUND" && opponentAttack == "WATER") {
+      gameResult = "LOST 😩"
+   } else if(playerAttack == "GROUND" && opponentAttack == "FIRE") {
+      gameResult = "WIN 🏆"
+   } else {
+      gameResult = "A RESULT IS MISSING"
+   }
+   createMessage()
+}
 
 window.addEventListener("load", startGame)// para que no importe la posicion de la etiqueta script en el html
+
+//game-result
